022b40ab62e45cdb24946a28b39b3c221a5634d6
David Alderson <dsa027@gmail.com>
2018-03-29 16:52:34 -0700

    Initial commit

---
+++ data/facts-en.js
@@ -0,0 +1,65 @@
+(function () {
+  const data = [
+    "Lake Powell shoreline is nineteen-hundred miles in length. That's more than twice as far as the California coast line.",
+    "Arizona is larger than the England, Wales, Scotland, and Northern Ireland combined.",
+    "The best preserved meteor crater in the world is in Winslow, Arizona. Yes, it is such a fine sight to see.",
+    "Skydive Arizona is the largest skydiving center in the world. ",
+    "You can stack the four tallest buildings in the United States on top of each other and they just barely peek over the rim of the Grand Canyon.",
+    "The roof of the Capitol building of Arizona has a copper roof equivalent to 4.8 million pennies",
+    "Arizona State University's has a larger student population than the population of Flagstaff.",
+    "Arizona's name originated from the Spanish name, Arizonac, derived from an Indian word meaning 'small spring'",
+    "Arizona is the largest copper producing state in the Unites States, producing more than all other states combined.",
+    "On June 29, nineteen-ninety-four, Lake Havasu City hit Arizona's all-time record high temperature at 128 Farenheit. That temperature is the highest of any city or town in the Western Hemishphere",
+    "Arizona is home to a majority of the Navajo Nation, the largest Native American reservation in the Unites States. The reservation covers 27,425 square miles of Arizona, Utah, and New Mexico.",
+    "Arizona has the most monuments of any state at 18 national monuments",
+    "Arizona is one of only two states that don’t observe Daylight Saving Time. The other is Hawaii.",
+    "Tombstone, Arizona has the world’s largest rose bush. The rose bush has a circumference of 12 feet, its trunk covers an area of 9,000 square feet and was planted in 1885.",
+    "It can take 100 years for a Saguaro cactus to grow an arm in areas of low precipitation",
+    "Pluto was discovered at the Lowell Observatory in Flagstaff, Arizona in 1930 by Clyde Tombaugh",
+    "Like margaritas? Thank Arizona! Nogales produced the country's first barrel of tequila in nineteen-thirty-six.",
+    "Arizona is the last contiguous state to join the union as the 48th state on Valentines Day, February 14th, nineteen-twelve",
+    "Arizona has 3,928 mountain peaks and summits, more than any one of the other Mountain States",
+    "Arizona is home to the largest solar telescope, located at the Kitts Peak National Observatory",
+    "The state capital and largest city, Phoenix, was founded in 1867 by Jack Swilling, a Confederate veteran of the Civil War",
+    "Mount Lemmon in the Santa Catalina Mountains is the southernmost ski resort in the United States",
+    "When England’s famous London Bridge was replaced in the 1960s, the original was purchased for two-and-a-half million dollars, dismantled, shipped stone by stone and reconstructed in Lake Havasu City, Arizona, where it still stands today",
+    "The only place in the country where mail is delivered by mule is the village of Supai, located at the bottom of the Grand Canyon",
+    "The Phoenix metropolitan area totals 14,598 square miles, one of the largest metropolitan areas in terms of area. That's enough to fit over 4 Londons, 19 New York City's, and 64 Chicagos.",
+    "Parker Dam in Arizona is the deepest dam in the world with three-quarters of it's 320-foot-high structure being below the original riverbed",
+    "Arizona is the sixth largest state in area, yet only about 17 percent of it is privately owned. The rest is public forest and park lands, state trust lands, and Native American reservations.",
+    "Palo Verde Nuclear Generating Station generates more electricity than any other U.S. power plant",
+    "The Sonoran Desert is the most biologically diverse desert in North America",
+    "The Meat Puppets, Jimmy Eats World, and The Gin Blossoms, among others, were from Arizona",
+    "Arizona is the home of the two largest manmade lakes in the U.S., Lake Mead and Lake Powell",
+    "The legendary 1881 Gunfight at the O.K. Corral in the town of Tombstone is considered the most famous shootout in the American Old West, and lasted only 30 seconds",
+    "The Navajo Nation is larger than Maryland, Delaware, New Jersey, and New York City combined.",
+    "Arozona's Four Corners is the only location in the United States where four states, Colorado, New Mexico, Arizona, and Utah intersect",
+    "You could face a year in prison if you cut down an endangered cactus like this Saguaro.",
+    "685 people have died at the Grand Canyon. Some of these are from natural causes.",
+    "According to the National Weather Service, the biggest snowfall ever recorded in Phoenix measured one inch in nineteen-thirty-three and nineteen-thirty-seven. We've been waiting ever since.",
+    "Eight men fell to their death into the Grand Canyon while hopping from one rock to another to pose for a picture",
+    "Women in Arizona were granted the right to vote eight years before national suffrage",
+    "There aren’t any dinosaur fossils in the Grand Canyon because it's a great deal older than dinosaurs. You'll only find fossils from corals, sponges, trilobites, and the like.",
+    "In nineteen-fifty-six, two planes detoured over the Grand Canyon for a better view. They collided over the canyon. The FAA was created in nineteen-fifty-eight as a result.",
+    "Arizona has 13 species of rattlesnakes, more than any other state",
+    "Possibly Arizona's most famous criminal, Ernesto Miranda, is the man responsible for mandated Miranda laws",
+    "Arizona hens lay about 5.8 billion eggs each year.",
+    "The Arizona Cardinals are the longest running continuous franchise in the NFL, dating back to eighteen-ninety-eight",
+    "The sun shines in Phoenix and Tucson 85 percent of the year, more than in Florida and Hawaii",
+    "Arizona has almost double the amount of wilderness area as the entire Midwest",
+    "For several hundred dollars a night, you can sleep 22 stories underground in the Grand Canyon Caverns in Peach Springs",
+  ];
+
+  const SKILL_NAME = "Arizona Facts";
+  const GET_FACT_MESSAGE = "Here's a fact: ";
+  const HELP_MESSAGE = "You can say 'tell me an Arizona fact', or, you can say exit... What can I help you with?";
+  const HELP_REPROMPT = "What can I help you with?";
+  const STOP_MESSAGE = "Goodbye!";
+})();
+
+module.exports.data = data;
+module.exports.SKILL_NAME = SKILL_NAME;
+module.exports.GET_FACT_MESSAGE = GET_FACT_MESSAGE;
+module.exports.HELP_MESSAGE = HELP_MESSAGE;
+module.exports.HELP_REPROMPT = HELP_REPROMPT;
+module.exports.STOP_MESSAGE = STOP_MESSAGE;
